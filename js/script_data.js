cb65ae222224fce634bcd1e87f15f1124616f22b
jorge <atientas0412@gmail.com>
2020-12-29 18:45:50 +0100

    script_data.js

--- js/script_data.js
+++ js/script_data.js
@@ -1,1 +1,24 @@
-console.log("ciao desde script_data.js");
+//console.log("ciao desde script_data.js");
+
+function caricaJSON() {
+
+    fetch('js/dati_lab.json')
+        .then((res) => {
+            //console.log(res);
+            return res.json()
+        })
+        .then(data => {
+            console.log(data.titoli_a_principali);
+
+            let html = '';
+            data.titoli_a_principali.forEach(function (titolo) {
+                html += `
+            <li><a href="#${titolo.titolo}">${titolo.titolo}</a></li>
+            `;
+            })
+            document.getElementById('menu').innerHTML = html;
+        })
+        .catch(error => console.log(error));
+}
+
+caricaJSON();
