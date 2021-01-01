f9a5d9041a8cb559def5caf55c029ac325a2b38c
Arianna <arianna102008@gmail.com>
2021-01-01 22:48:11 +0100

    aggiunta paragrafi coni dati json

---
+++ custom/script_data.js
@@ -0,0 +1,105 @@
+//console.log("ciao desde script_data.js");
+
+function caricaJSON() {
+
+    fetch('custom/json/dati_lab.json')
+        .then((res) => {
+            //console.log(res);
+            return res.json()
+        })
+        .then(data => {
+            carica_titoli_a_principali(data);
+            carica_titoli_home(data);
+            caricare_paragrafi(data);
+
+        })
+        .catch(error => console.log(error));
+}
+
+caricaJSON();
+
+
+function carica_titoli_a_principali(data) {
+    let html = '';
+    data.home.titoli_a_principali.forEach(function (item) {
+
+        if (item.titolo == "home") {
+            html += `<li class="active"><a href="#${item.titolo}">${item.titolo}</a></li>`;
+        } else {
+            html += `<li><a href="#${item.titolo}">${item.titolo}</a></li>`;
+        }
+    });
+    document.getElementById('menu').innerHTML = html;
+
+}
+
+function carica_titoli_home(data) {
+    console.log(data.home.titoli_home);
+    let html = '';
+
+    data.home.titoli_home.forEach(function (item) {
+        console.log(item.titolo);
+
+        var div = document.getElementById("camera_wrap_1")
+
+        html += `<div data-thumb="" data-src="images/slides/blank.gif">
+								<div class="img-responsive camera_caption fadeFromBottom">
+									<h2>${item.titolo}</h2>
+								</div>
+				 </div>`;
+
+
+        div.innerHTML = html;
+
+        jQuery(function () {
+            jQuery('#camera_wrap_1').camera({
+                transPeriod: 500,
+                time: 3000,
+                height: '490px',
+                thumbnails: false,
+                pagination: true,
+                playPause: false,
+                loader: false,
+                navigation: false,
+                hover: false
+            });
+        });
+
+    });
+}
+
+function caricare_paragrafi(data) {
+    let html = '';
+    var conteniore_paragrafo = document.getElementById("paragrafo");
+    console.log(data);
+
+    data.home.quantita_X.forEach(function (item, index) {
+
+        html += `<div class="col-md-4 project">
+                    <h3 id="counter${index}">${item.numero}</h3>
+                    <h4>${item.titolo}</h4>
+                    <p>${item.testo}</p>
+                </div>`;
+
+
+        conteniore_paragrafo.innerHTML = html;
+
+
+    })
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
