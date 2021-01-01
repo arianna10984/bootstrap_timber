d5035def708e5545782cd6c6a8d8171fe710b815
jorge <atientas0412@gmail.com>
2021-01-01 23:19:28 +0100

    resuelto problema scroll

--- custom/script_data.js
+++ custom/script_data.js
@@ -31,6 +31,15 @@ function carica_titoli_a_principali(data) {
     });
     document.getElementById('menu').innerHTML = html;
 
+    calculateScroll();
+    $(window).scroll(function (event) {
+        calculateScroll();
+    });
+    $('.navmenu ul li a').click(function () {
+        $('html, body').animate({scrollTop: $(this.hash).offset().top - 80}, 800);
+        return false;
+    });
+
 }
 
 function carica_titoli_home(data) {
@@ -81,12 +90,29 @@ function caricare_paragrafi(data) {
                     <p>${item.testo}</p>
                 </div>`;
 
-
         conteniore_paragrafo.innerHTML = html;
-
 
     })
 
+}
+
+function calculateScroll() {
+    var contentTop = [];
+    var contentBottom = [];
+    var winTop = $(window).scrollTop();
+    var rangeTop = 200;
+    var rangeBottom = 500;
+    $('.navmenu').find('a').each(function () {
+        contentTop.push($($(this).attr('href')).offset().top);
+        contentBottom.push($($(this).attr('href')).offset().top + $($(this).attr('href')).height());
+    })
+    $.each(contentTop, function (i) {
+        if (winTop > contentTop[i] - rangeTop && winTop < contentBottom[i] - rangeBottom) {
+            $('.navmenu li')
+                .removeClass('active')
+                .eq(i).addClass('active');
+        }
+    })
 }
 
 
